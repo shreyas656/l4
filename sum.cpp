45ccabd2dddd40beabe7ee55a776555d6b2d919d
unknown <shreyas.m@s.amity.edu>
2025-02-24 11:36:03 +0530

    c program

---
+++ sum.cpp
@@ -0,0 +1,19 @@
+To find sum of 2 numbers
+
+#include <stdio.h>
+int main() {
+    int num1, num2, sum;
+
+    printf("Enter the first integer: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second integer: ");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+
+    printf("The sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;
+}
+
